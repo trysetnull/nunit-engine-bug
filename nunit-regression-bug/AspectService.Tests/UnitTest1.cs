b46935d3094268f1a25b85eedafb901a4606dd3f
Tully Ernst <3741862+trysetnull@users.noreply.github.com>
2024-12-03 14:20:35 +0000

    feat: demonstrate .NET Core has exactly one AppDomain
    See: https://learn.microsoft.com/en-us/dotnet/api/system.appdomain?view=net-8.0#remarks

--- nunit-regression-bug/AspectService.Tests/UnitTest1.cs
+++ nunit-regression-bug/AspectService.Tests/UnitTest1.cs
@@ -5,7 +5,7 @@ using AspectInterfaces;
 
 [TestFixture]
 [NUnitAspect]
-public class Tests: IPropertyReceiver
+public class Tests : IPropertyReceiver
 {
     public string? BeforeTest { get; set; }
 
@@ -13,5 +13,31 @@ public class Tests: IPropertyReceiver
     public void FailsWithNUnitEngine3_18_3()
     {
         Assert.That(BeforeTest, Is.EqualTo(NUnitAspectAttribute.ExpectedValue));
+    }
+
+    [Test]
+    public void ListAssembliesInCurrentAppDomain()
+    {
+        // Arrange
+        var expectedAssemblies = new[] {
+            "AspectInterfaces",
+            "AspectService",
+            "AspectService.Tests",
+            "nunit.engine",
+            "nunit.engine.api",
+            "nunit.engine.core",
+            "nunit.framework",
+            "testcentric.engine.metadata",
+        };
+
+        // Act
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(AppDomain.CurrentDomain.IsDefaultAppDomain(), Is.True);
+            Assert.That(assemblies.Select(assembly => assembly.GetName().Name), Is.SupersetOf(expectedAssemblies));
+        });
     }
 }
--- nunit-regression-bug/AspectService/Program.cs
+++ nunit-regression-bug/AspectService/Program.cs
@@ -1,5 +1,7 @@
 namespace AspectService;
 
+using System.Reflection;
+using System.Runtime.Loader;
 using System.Xml;
 using AspectInterfaces;
 using NUnit.Engine;
@@ -18,14 +20,36 @@ internal sealed class Program
 
         var fileInfo = new FileInfo(args[0]);
 
-        System.Reflection.Assembly.LoadFrom(fileInfo.FullName);
+        // Load into the default AssemblyLoadContext.
+        var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(fileInfo.FullName);
+        var testType = assembly.GetType("AspectService.Tests.Tests");
+        var attribute = testType!.GetCustomAttribute<NUnitAspectAttribute>();
+        var attributeType = attribute!.GetType();
+
+        if (attributeType != null)
+        {
+            var staticProperty = attributeType.GetProperty("StaticProperty", BindingFlags.Public | BindingFlags.Static);
+            if (staticProperty != null)
+            {
+                var staticPropertyValue = staticProperty.GetValue(null);
+                Console.WriteLine($"NUnitAspectAttribute.StaticProperty: [{staticPropertyValue}]");
+            }
+            else
+            {
+                Console.WriteLine("StaticProperty not found on NUnitAspectAttribute.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("NUnitAspectAttribute type not found in the assembly.");
+        }
 
         using var engine = new TestEngine();
         engine.WorkDirectory = fileInfo.DirectoryName;
         engine.InternalTraceLevel = InternalTraceLevel.Debug;
         engine.Initialize();
 
-        TestPackage package = new(fileInfo.FullName);
+        TestPackage package = new(assembly.Location);
         var runner = engine.GetRunner(package);
 
         try
